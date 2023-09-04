7d7d6a11a341fad827f6ac2984f039b874c06031
anatolii_kim <anatolii_kim@epam.com>
2023-09-04 15:00:11 +0500

    feat: SearchForm component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Counter from './components/counter/Counter';
+import SearchForm from './components/searchForm/SearchForm';
 
 function App() {
   return (
-    <div className="App">
-      <Counter count={1}/>
+    <div className="app">
+      <div className='content-restrict'>
+        <Counter count={1}/>
+        <SearchForm className="search" initialQuery={''} onSearch={() => alert('Form was submitted')}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/searchForm/SearchForm.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import './SearchForm.css';
+
+export default function SearchForm({ initialQuery, onSearch }) {
+  const [searchQuery, setSearchQuery] = useState(initialQuery);
+
+  const handleChange = (event) => {
+    setSearchQuery(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    onSearch(searchQuery);
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="search-form">
+      <input 
+        type="text" 
+        value={searchQuery} 
+        onChange={handleChange} 
+        placeholder="What do you want to watch?" 
+        className="search-input"
+      />
+      <input type="submit" value="Submit" className="submit"/>
+    </form>
+  );
+}
