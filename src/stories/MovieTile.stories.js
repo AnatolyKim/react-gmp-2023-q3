98bb21705d6472478b8f5d096a974882af4f9e33
anatolii_kim <anatolii_kim@epam.com>
2023-09-19 09:18:41 +0500

    feat: SortControl, MovieTiles and MovieDetails components implemented

---
+++ src/stories/MovieTile.stories.js
@@ -0,0 +1,27 @@
+import MovieTile from '../components/movieTile/MovieTile';
+
+export default {
+  title: 'App/MovieTile',
+  component: MovieTile,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    releaseYear: { control: 'text' },
+    name: { control: 'text' },
+    imageUrl: { control: 'text' },
+  },
+};
+
+export const Default = {
+  args: {
+    movie: {
+      imageUrl: '../assets/movie-posters/pulp_fiction.png',
+      name: 'Pulp Fiction',
+      releaseYear: 2023,
+      genres: ['Action', 'Adventure'],
+    },
+    onClick: (movie) => console.log(movie)
+  },
+};
---
+++ src/stories/SortControl.stories.js
@@ -0,0 +1,23 @@
+import SortControl from '../components/sortControl/SortControl';
+
+export default {
+  title: 'App/SortControl',
+  component: SortControl,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    currentSelection: { 
+      control: { type: 'select' },
+      options: ['release-date', 'title'],
+    },
+  },
+};
+
+export const Default = {
+  args: {
+    currentSelection: 'release-date',
+    onSelectionChange: (type) => console.log(type)
+  },
+};
