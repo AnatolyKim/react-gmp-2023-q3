96207aa3718ff5a95a3089f1bcd174f418a5930a
anatolii_kim <anatolii_kim@epam.com>
2023-10-09 19:40:51 +0500

    feat: dialog component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css';
-import Counter from './components/counter/Counter';
-import SearchForm from './components/searchForm/SearchForm';
-import GenreSelect from './components/genreSelect/GenreSelect';
-import MovieTile from './components/movieTile/MovieTile';
-import MovieDetails from './components/movieDetails/MovieDetails';
-import SortControl from './components/sortControl/SortControl';
+import Counter from './components/counter';
+import SearchForm from './components/searchForm';
+import GenreSelect from './components/genreSelect';
+import MovieTile from './components/movieTile';
+import MovieDetails from './components/movieDetails';
+import SortControl from './components/sortControl';
 import { useState } from 'react';
+import Dialog from './components/dialog';
+import MovieForm from './components/movieForm';
 
 function App() {
   const [genres] = useState(["action", "comedy", "drama", "horror", "sci-fi"]);
   const [selectedGenre] = useState(genres[0]);
+  const [open, setOpen] = useState(false);
+  
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const movieExample = {
     imageUrl: '../assets/movie-posters/pulp_fiction.png',
     name: 'Pulp Fiction',
@@ -17,6 +25,7 @@ function App() {
     genres: ['Action', 'Adventure'],
     rating: 8.9,
     duration: 154,
+    url: 'https://movie-url.com',
     description: "Jules Winnfield (Samuel L. Jackson) and Vincent Vega (John Travolta) are two hit men who are out to retrieve a suitcase stolen from their employer, mob boss Marsellus Wallace (Ving Rhames). Wallace has also asked Vincent to take his wife Mia (Uma Thurman) out a few days later when Wallace himself will be out of town. Butch Coolidge (Bruce Willis) is an aging boxer who is paid by Wallace to lose his fight. The lives of these seemingly unrelated people are woven together comprising of a series of funny."
   }
 
@@ -28,18 +37,25 @@ function App() {
     console.log(`Following genre selected: ${genre}`);
   }
 
+  const handleMovieSubmit = (movie) => {
+    console.log(`Movie info: ${movie}`);
+  }
+
   return (
-    <div className="app" data-testid="app">
+    <div className="app">
       <div className='content-restrict'>
         <h1>Learn react</h1>
+        <MovieForm initialMovieInfo={movieExample} genreInfo={{genres: movieExample.genres, selectedGenre: movieExample.genres[0]}} onSubmit={handleMovieSubmit}></MovieForm>
         <Counter count={1}/>
         <SearchForm className="search" initialQuery={''} onSearch={onSearchSubmit}/>
-        <GenreSelect genres={genres} selectedGenre={selectedGenre} onSelect={onGenreSelect} />
         <SortControl currentSelection={'release-date'} onSelectionChange={(sortType) => console.log('Sorting type:', sortType)}/>
         <div className='divider'></div>
         <MovieTile movie={movieExample} onClick={(movie) => console.log('Clicked movie:', movie)} />
         <div className='divider'></div>
         <MovieDetails movie={movieExample} />
+        {open &&<Dialog title="My Dialog" onClose={handleClose}>
+          <GenreSelect genres={genres} selectedGenre={selectedGenre} onSelect={onGenreSelect} />
+        </Dialog>}
       </div>
     </div>
   );
---
+++ src/components/dialog/Dialog.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Dialog from './index';
+
+describe('Dialog', () => {
+  it('renders the title and children', () => {
+    render(
+      <Dialog title="Test Title" onClose={() => {}}>
+        <form>
+          <input type="text" name="title" className="input" tabIndex="0"/>
+          <button tabIndex="0">Submit</button>
+        </form>
+      </Dialog>
+    );
+
+    expect(screen.getByText('Test Title')).toBeInTheDocument();
+    expect(screen.getByText('Submit')).toBeInTheDocument();
+  });
+
+  it('calls onClose when close button is clicked', () => {
+    const onCloseMock = jest.fn();
+    render(
+      <Dialog title="Test Title" onClose={onCloseMock}>
+        <div>
+          <input type="text" name="title" className="input" tabIndex="0"/>
+          <button tabIndex="0">Submit</button>
+        </div>
+      </Dialog>
+    );
+
+    const closeButton = screen.getByTestId('close');
+    fireEvent.click(closeButton);
+
+    expect(onCloseMock).toHaveBeenCalled();
+  });
+});
