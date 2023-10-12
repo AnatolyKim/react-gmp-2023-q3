a5216c99b60bc579b3ea08f950009f03a71c4e77
anatolii_kim <anatolii_kim@epam.com>
2023-10-12 21:14:05 +0500

    feat: main page functionality implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,92 @@
-import './App.css';
-import Counter from './components/counter';
+import styles from './styles.module.css';
 import SearchForm from './components/searchForm';
-import GenreSelect from './components/genreSelect';
 import MovieTile from './components/movieTile';
-import MovieDetails from './components/movieDetails';
 import SortControl from './components/sortControl';
-import { useState } from 'react';
-import Dialog from './components/dialog';
-import MovieForm from './components/movieForm';
+import MovieDetails from './components/movieDetails';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [genres] = useState(["action", "comedy", "drama", "horror", "sci-fi"]);
-  const [selectedGenre] = useState(genres[0]);
-  const [open, setOpen] = useState(false);
-  
-  const handleClose = () => {
-    setOpen(false);
-  };
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('');
+  const [sorting, setSorting] = useState('release_date');
+  const [activeGenre, setActiveGenre] = useState('All');
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [moviesCount, setMoviesCount] = useState(0);
+  const genreFilters = [ 'All', 'Comedy', 'Drama', 'Action', 'Romance'];
 
-  const movieExample = {
-    imageUrl: '../assets/movie-posters/pulp_fiction.png',
-    name: 'Pulp Fiction',
-    releaseYear: 1994,
-    genres: ['Action', 'Adventure'],
-    rating: 8.9,
-    duration: 154,
-    url: 'https://movie-url.com',
-    description: "Jules Winnfield (Samuel L. Jackson) and Vincent Vega (John Travolta) are two hit men who are out to retrieve a suitcase stolen from their employer, mob boss Marsellus Wallace (Ving Rhames). Wallace has also asked Vincent to take his wife Mia (Uma Thurman) out a few days later when Wallace himself will be out of town. Butch Coolidge (Bruce Willis) is an aging boxer who is paid by Wallace to lose his fight. The lives of these seemingly unrelated people are woven together comprising of a series of funny."
-  }
+  useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+    let queryParams = `limit=6&searchBy=title&sortOrder=asc&sortBy=${sorting}`;
 
-  const onSearchSubmit = (query) => {
-    console.log(`Search initiated for: ${query}`);
-  }
+    if (query) {
+      queryParams = queryParams.concat(`&search=${query}`);
+    }
 
-  const onGenreSelect = (genre) => {
-    console.log(`Following genre selected: ${genre}`);
-  }
+    if (activeGenre !== 'All') {
+      queryParams = queryParams.concat(`&filter=${activeGenre}`);
+    }
 
-  const handleMovieSubmit = (movie) => {
-    console.log(`Movie info: ${movie}`);
+    fetch(`http://localhost:4000/movies?${queryParams}`, { signal })
+      .then((res) => res.json())
+      .then((res) => {
+        setMovies(res.data);
+        setMoviesCount(res.totalAmount);
+      })
+      .catch((err) => console.warn(err));
+    
+    return () => abortController.abort();
+  }, [query, sorting, activeGenre]);
+
+  const mapMovieData = (movie) => {
+    const { title, poster_path, vote_average, release_date, overview, genres, runtime } = movie;
+
+    return {
+      genres,
+      name: title,
+      imageUrl: poster_path,
+      rating: vote_average,
+      releaseYear: release_date.slice(0, 4),
+      description: overview,
+      duration: runtime,
+    }
   }
 
   return (
-    <div className="app">
-      <div className='content-restrict'>
-        <h1>Learn react</h1>
-        <MovieForm initialMovieInfo={movieExample} genreInfo={{genres: movieExample.genres, selectedGenre: movieExample.genres[0]}} onSubmit={handleMovieSubmit}></MovieForm>
-        <Counter count={1}/>
-        <SearchForm className="search" initialQuery={''} onSearch={onSearchSubmit}/>
-        <SortControl currentSelection={'release-date'} onSelectionChange={(sortType) => console.log('Sorting type:', sortType)}/>
-        <div className='divider'></div>
-        <MovieTile movie={movieExample} onClick={(movie) => console.log('Clicked movie:', movie)} />
-        <div className='divider'></div>
-        <MovieDetails movie={movieExample} />
-        {open &&<Dialog title="My Dialog" onClose={handleClose}>
-          <GenreSelect genres={genres} selectedGenre={selectedGenre} onSelect={onGenreSelect} />
-        </Dialog>}
+    <div className={styles.app}>
+      <header className={styles.header}>
+        <div className={`${styles.contentRestrict} ${styles.navigation}`}>
+          <div className={styles.logo}></div>
+          {selectedMovie 
+            ? <button className={styles.search} onClick={() => setSelectedMovie(null)}></button>
+            : <button className={styles.addMovie}>+ Add Movie</button>
+          }
+          {selectedMovie 
+            ? <MovieDetails movie={selectedMovie}></MovieDetails>
+            : <div className={styles.searchForm}>
+                <h1 className={styles.title}>Find your movie</h1>
+                <SearchForm className="search" initialQuery={''} onSearch={(query) => setQuery(query)}/>
+              </div>
+          }
+        </div>
+      </header>
+      <div className={styles.contentRestrict}>
+        <div className={styles.filters}>
+          <ul className={styles.genres}>
+            {genreFilters.map((genre) =>(
+              <li className={`${styles.genre} ${activeGenre === genre && styles.active}`} key={genre} onClick={() => setActiveGenre(genre)}>{genre}</li>
+            ))}
+          </ul>
+          <SortControl currentSelection={'release-date'} onSelectionChange={(sortType) => setSorting(sortType)}/>
+        </div>
+        <div className={styles.totalCount}><b>{moviesCount}</b> movies found</div>
+        <div className={styles.movieList}>
+          {movies.map((movie) => (
+            <MovieTile movie={mapMovieData(movie)} key={movie.title} onClick={() => setSelectedMovie(mapMovieData(movie))} />
+          ))}
+        </div>
       </div>
+      <footer className={styles.footer}></footer>
     </div>
   );
 }
